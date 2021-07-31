21d17c1834088e324d9927d2b2430caa6073995f
ArnoldPires <arnold.pires92@gmail.com>
2021-07-31 10:23:12 -0400

    added hero

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
+//Nav for moblie
 const getElement = (selector) => {
     const element = document.querySelector(selector);
     if(element) return element
-    throw Error(`Please double check your class names, there is no ${selector} class`)
+    throw Error(`Please double check your class names, there is no ${selector} class`);
 }
-const links = getElement('.nav-links')
+const links = getElement('.nav-links');
 const navBtnDom = getElement(".nav-btn");
 
 navBtnDom.addEventListener('click',()=> {
-    links.classList.toggle('show-links')
+    links.classList.toggle('show-links');
 })
+
+//Date
+const date = getElement('#date');
+const currentYear = new Date().getFullYear();
+date.textContent = currentYear;
